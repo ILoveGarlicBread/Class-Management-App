4bf4364121654b27f17e075114c6559773b8f2ee
khoinopro <135790463+khoinopro@users.noreply.github.com>
2024-11-18 09:46:35 +0700

    Update Student.java

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,13 +1,16 @@
 package model;
 
-public final class Student extends Person {
+public class Student extends Person {
+  private static int studentCount = 0; // Static variable to track number of students
   public Student() {
+    studentCount++; // Increment the count whenever a new Student object is created
   }
 
   public Student(String name, int ID, String email) {
     this.name = name;
     this.ID = ID;
     this.email = email;
+    studentCount++;
   }
   public final void viewInfo(){
       super.viewInfo();
@@ -15,5 +18,8 @@ public final class Student extends Person {
   public final void updateInfo(String name, String email, String password, int ID){
     super.updateInfo(name, email, password, ID);
   }
+  // Static method to access the studentCount
+  public static int getStudentCount() {
+    return studentCount;
+  }
 }
-
