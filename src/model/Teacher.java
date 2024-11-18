3e1695aa3e57e73987606dae9251a62acf8b7320
khoinopro <135790463+khoinopro@users.noreply.github.com>
2024-11-18 09:29:53 +0700

    Update Teacher.java

--- src/model/Teacher.java
+++ src/model/Teacher.java
@@ -1,24 +1,36 @@
 package model;
 
 public class Teacher extends Person {
-
+  private final int age;
   public Teacher() {
+    this.age= 0;
   };
 
-  public Teacher(String name, int ID) {
+  public Teacher(String name, int ID, int age) {
     this.name = name;
     this.ID = ID;
+    this.age= age;
   }
 
-  public Teacher(String name, int ID, String email, String password) {
+  public Teacher(String name, int ID, String email, String password, int age) {
     this.name = name;
     this.ID = ID;
     this.email = email;
     this.password = password;
+    this.age= age;
   }
+  public final void viewInfo(){
+    super.viewInfo();
+    System.out.println("Age of teacher: "+ age);
 
+  }
+  public final void updateInfo(String name, String email, String password, int ID){
+    super.updateInfo(name, email, password, ID);
+  }
   public String getPassword() {
     return password;
   }
-
+  public int getAge() {
+    return age;
+  }
 }
